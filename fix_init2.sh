aa1a0f535e3281327d3762484d6e9f650e348bf4
tecnicablepromotores-sys <tecnicablepromotores@gmail.com>
2026-08-03 12:57:44 -0400

    Delete fix_init2.sh

--- fix_init2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sed -i '/val isUserLoggedIn: StateFlow<Boolean> = _isUserLoggedIn.asStateFlow()/a \
-    private val _isUserAuthenticated = MutableStateFlow(false)\
-    val isUserAuthenticated: StateFlow<Boolean> = _isUserAuthenticated.asStateFlow()' app/src/main/java/com/example/MainViewModel.kt
-
-sed -i '/val currentUser = auth.currentUser/a \
-            _isUserAuthenticated.value = currentUser != null' app/src/main/java/com/example/MainViewModel.kt
