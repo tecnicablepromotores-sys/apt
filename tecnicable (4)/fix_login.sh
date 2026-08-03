fcb52b2c5b0f0eb0d78dad641750ef16b026d26b
tecnicablepromotores-sys <tecnicablepromotores@gmail.com>
2026-08-03 12:57:34 -0400

    Delete tecnicable (4) directory

--- tecnicable (4)/fix_login.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i 's/viewModel.signInWithFirebaseCredential(credential)/viewModel.loginWithGoogle(credential)/g' app/src/main/java/com/example/ui/screens/TecnicableLoginScreen.kt
-sed -i 's/viewModel.attemptLoginAsync(usuario, clave)/viewModel.login(usuario, clave)/g' app/src/main/java/com/example/ui/screens/TecnicableLoginScreen.kt
